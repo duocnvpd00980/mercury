59d90c9b2e5349349bdc59154ad99344313b3ec6
Duoc <lytuongdn@gmail.com>
2024-07-23 21:46:13 +0700

    up 22222222

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
 
 export default async function Home() {
-  const res = await fetch(`http://localhost:3000/api`);
-  const data = await res.json();
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello World {data.message}</h2>
+      <h2>Hello World</h2>
     </main>
   );
 }
