207d8bbf56643f28708dd62296b7fe530f70d31b
Duoc <lytuongdn@gmail.com>
2024-07-23 21:02:06 +0700

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const res = await fetch(`${process.env.API_URL}/api/hello`);
-  const data = await res.json();
+ 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello World {data.message}</h2>
+      <h2>Hello World</h2>
     </main>
   );
 }
